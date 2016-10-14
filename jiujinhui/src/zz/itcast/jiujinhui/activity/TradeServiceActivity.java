6325faa1886c650f9c4da70d3cc06a3a3856872a
JJHLi <JJHLi@192.168.1.112>
2016-10-14 18:00:51 +0800

    初处理买入卖出按钮

--- jiujinhui/src/zz/itcast/jiujinhui/activity/TradeServiceActivity.java
+++ jiujinhui/src/zz/itcast/jiujinhui/activity/TradeServiceActivity.java
@@ -16,65 +16,92 @@ import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 public class TradeServiceActivity extends BaseActivity {
-      
-	//交易曲线
+	// 买入
+	@ViewInject(R.id.rb_buy_service)
+	private RadioButton rb_buy_service;
+	// 卖出
+	@ViewInject(R.id.rb_sale_service)
+	private RadioButton rb_sale_service;
+
+	// 提货
+	@ViewInject(R.id.rb_tihuo_service)
+	private RadioButton rb_tihuo_service;
+	// 转让
+	@ViewInject(R.id.rb_zhuanrang_service)
+	private RadioButton rb_zhuanrang_service;
+
+	// 交易曲线
 	@ViewInject(R.id.tabs)
 	private PagerSlidingTabStrip tabs;
 	@ViewInject(R.id.trade_pager)
 	private ViewPager trade_pager;
-	
+
 	//
-	//交易曲线
+	// 交易曲线
 	private ArrayList<Fragment> fragmentsList1;
-	//买入卖出曲线
+	// 买入卖出曲线
 	@ViewInject(R.id.tabs_buysale)
 	private PagerSlidingTabStrip tabs_buysale;
 	@ViewInject(R.id.buy_sale_pager)
 	private ViewPager buy_sale_pager;
-	
-		private ArrayList<Fragment> fragmentsList2;
+
+	private ArrayList<Fragment> fragmentsList2;
 	@ViewInject(R.id.hscrollview)
 	private HorizontalScrollView hscrollview;
 	@ViewInject(R.id.ll_scroll)
-	
 	private LinearLayout ll_scroll;
 	@ViewInject(R.id.tv_back)
 	private ImageView tv_back;
 	@ViewInject(R.id.tv__title)
 	private TextView tv__title;
-     //定义一个Handler对象
-	private final Handler handler=new Handler();
-	//当前滚动距离
-	int currentX=0;
-	
-	//实现滚动线程
-	private Runnable hscrollRunnable=new Runnable() {
+	// 定义一个Handler对象
+	private final Handler handler = new Handler();
+	@Override
+	public void initListener() {
+		// TODO Auto-generated method stub
+		tv_back.setOnClickListener(this);
+		rb_buy_service.setOnClickListener(this);
+		rb_sale_service.setOnClickListener(this);
+		rb_tihuo_service.setOnClickListener(this);
+		rb_zhuanrang_service.setOnClickListener(this);
 		
+		
+	}
+
+	// 当前滚动距离
+	int currentX = 0;
+
+	// 实现滚动线程
+	private Runnable hscrollRunnable = new Runnable() {
+
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			//判断宽度
-			int off=ll_scroll.getMeasuredWidth();
-		
+			// 判断宽度
+			int off = ll_scroll.getMeasuredWidth();
+
 			hscrollview.scrollBy(off, 0);
-			
+
 			handler.postDelayed(this, 4000);
-			
+
 		}
 	};
+
 	@Override
 	public void initView() {
 		// TODO Auto-generated method stub
-         ViewUtils.inject(this);
-         tv__title.setText("交易服务");
-         handler.post(hscrollRunnable);
+		ViewUtils.inject(this);
+		tv__title.setText("交易服务");
+		handler.post(hscrollRunnable);
 	}
+
 	@Override
 	public int getLayoutResID() {
 		// TODO Auto-generated method stub
@@ -87,19 +114,20 @@ public class TradeServiceActivity extends BaseActivity {
 		fragmentsList1 = new ArrayList<Fragment>();
 		fragmentsList1.add(new NowTradeRecoedFragment());
 		fragmentsList1.add(new EveryDayTradeRecordFragment());
-		fragmentsList2=new ArrayList<Fragment>();
+		fragmentsList2 = new ArrayList<Fragment>();
 		fragmentsList2.add(new SaleChartFragment());
 		fragmentsList2.add(new BuyChartFragment());
 		trade_pager.setAdapter(new MypagerAdapter(getSupportFragmentManager(),
 				fragmentsList1));
-		buy_sale_pager.setAdapter(new MyBuySalepagerAdapter(getSupportFragmentManager(),
-				fragmentsList2));
+		buy_sale_pager.setAdapter(new MyBuySalepagerAdapter(
+				getSupportFragmentManager(), fragmentsList2));
 		tabs.setViewPager(trade_pager);
 		tabs_buysale.setViewPager(buy_sale_pager);
 		tabs.setShouldExpand(true);
 		tabs_buysale.setShouldExpand(true);
 	}
-   //交易曲线图适配器
+
+	// 交易曲线图适配器
 	public class MypagerAdapter extends FragmentPagerAdapter {
 		private ArrayList<Fragment> fragmentsList1;
 
@@ -112,7 +140,9 @@ public class TradeServiceActivity extends BaseActivity {
 			super(fm);
 			// TODO Auto-generated constructor stub
 		}
+
 		private final String[] titles1 = { "实时", "每天" };
+
 		@Override
 		public CharSequence getPageTitle(int position) {
 			// TODO Auto-generated method stub
@@ -132,11 +162,13 @@ public class TradeServiceActivity extends BaseActivity {
 		}
 
 	}
-	//买入卖出图标适配器
+
+	// 买入卖出图标适配器
 	public class MyBuySalepagerAdapter extends FragmentPagerAdapter {
 		private ArrayList<Fragment> fragmentsList2;
 
-		public MyBuySalepagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+		public MyBuySalepagerAdapter(FragmentManager fm,
+				ArrayList<Fragment> fragments) {
 			super(fm);
 			this.fragmentsList2 = fragments;
 		}
@@ -145,7 +177,9 @@ public class TradeServiceActivity extends BaseActivity {
 			super(fm);
 			// TODO Auto-generated constructor stub
 		}
+
 		private final String[] titles2 = { "卖出", "买入" };
+
 		@Override
 		public CharSequence getPageTitle(int position) {
 			// TODO Auto-generated method stub
@@ -165,25 +199,37 @@ public class TradeServiceActivity extends BaseActivity {
 		}
 
 	}
-	@Override
-	public void initListener() {
-		// TODO Auto-generated method stub
-      tv_back.setOnClickListener(this);
-	}
 
 	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-            switch (v.getId()) {
-			case R.id.tv_back:
-				finish();
-				break;
+		switch (v.getId()) {
+		case R.id.tv_back:
+			finish();
+			break;
+		case R.id.rb_buy_service:
+			
+			
+			
+			break;
+		case R.id.rb_sale_service:
+			
+			break;
 
-			default:
-				break;
-			}  
+		case R.id.rb_tihuo_service:
+			
+			break;
+
+		case R.id.rb_zhuanrang_service:
+			
+			break;
+
+			
+
+		default:
+			break;
+		}
 	}
-
 
 }
